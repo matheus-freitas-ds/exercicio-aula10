4dd21481dc0ad1f3795bb932f9fb24f3cbd0d75f
Matheus <matheusvmja@gmail.com>
2023-09-28 09:08:12 -0300

    Exercicio finalizado

--- enunciado__pequena-fabrica-2/tests/factories/user-factory.ts
+++ enunciado__pequena-fabrica-2/tests/factories/user-factory.ts
@@ -1,12 +1,11 @@
 import prisma from "../../src/database";
-import { UserInput } from "../../src/repository";
+import { faker } from '@faker-js/faker'
 
 export async function buildUser(email: string, password?: string) {
-  const userData: UserInput = {
-    email,
-    password: password || new Date().getTime().toString()
-  };
 
-  const user = await prisma.user.create({ data: userData });
+  const user = await prisma.user.create({ data: {
+    email: faker.internet.email(),
+    password: faker.internet.password()
+  } });
   return user;
 }
